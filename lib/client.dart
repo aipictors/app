56d770c2fdc818af3a0207f44ef404e45178ba40
n <reiwa@outlook.com.vn>
2023-06-04 01:58:37 +0900

    refactor

--- lib/client.dart
+++ lib/client.dart
@@ -1,14 +1,13 @@
 import 'package:ferry/ferry.dart';
 import 'package:gql_http_link/gql_http_link.dart';
 
+import 'config.dart';
 import 'global.dart';
 
 Client createClient() {
   final cache = Cache(store: Global.store!);
 
-  final link = HttpLink(
-    'https://prompton-ptiui4gkeq-an.a.run.app/api/xt99t8rfh5uhgtna3buj',
-  );
+  final link = HttpLink(Config.graphqlURI);
 
   final client = Client(
     link: link,
---
+++ lib/config.dart
@@ -0,0 +1,5 @@
+class Config {
+  static get graphqlURI {
+    return 'https://prompton-ptiui4gkeq-an.a.run.app/api/xt99t8rfh5uhgtna3buj';
+  }
+}
