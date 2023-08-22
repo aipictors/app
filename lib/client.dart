e4f05b197651d6786a1bfa920c768cb8b3295ef8
n <reiwa@outlook.com.vn>
2023-08-23 04:31:21 +0900

    update

--- lib/client.dart
+++ lib/client.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:aipictors/cache.dart';
 import 'package:aipictors/config.dart';
+import 'package:aipictors/repositories/config_repository.dart';
 import 'package:ferry/ferry.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:gql_http_link/gql_http_link.dart';
@@ -9,6 +12,8 @@ Future<Client> createClient() async {
 
   final token = await FirebaseAuth.instance.currentUser?.getIdToken(true);
 
+  final language = const ConfigRepository().language;
+
   final httpLink = HttpLink(
     DefaultConfig.graphqlEndpoint,
     defaultHeaders: {
@@ -16,6 +21,8 @@ Future<Client> createClient() async {
       'flutter_version': DefaultConfig.version,
       'flutter_build_number': DefaultConfig.buildNumber,
       'flutter_secret_token': DefaultConfig.secretToken,
+      'flutter_operating_system': Platform.operatingSystem.toUpperCase(),
+      'flutter_language': language.toUpperCase(),
     },
   );
 
