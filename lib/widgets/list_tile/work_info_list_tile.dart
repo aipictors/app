1d999929133350eb043177aae067b1e98805dcee
stella <stella221125@gmail.com>
2023-10-03 01:15:59 +0900

    update: 「あなたの作品」の表示をListViewにする

---
+++ lib/widgets/list_tile/work_info_list_tile.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:aipictors/widgets/container/list_tile_image_container.dart';
+
+/// 作品・シリーズ・コレクション等に利用できる、汎用ウィジェット
+class WorkInfoListTile extends HookConsumerWidget {
+  const WorkInfoListTile({
+    Key? key,
+    required this.thumbnailImageURL,
+    required this.title,
+    required this.body,
+    required this.onTap,
+  }) : super(key: key);
+
+  final String thumbnailImageURL;
+
+  final String title;
+
+  final Widget body;
+
+  final VoidCallback onTap;
+
+  @override
+  Widget build(context, ref) {
+    return ListTile(
+      leading: ListTileImageContainer(thumbnailImageURL: thumbnailImageURL),
+      title: Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
+      subtitle: body,
+      onTap: onTap,
+    );
+  }
+}
