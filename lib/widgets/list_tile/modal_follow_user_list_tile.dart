0d479a0a9bbe375e38f6ae9935175ad70a5963b2
stella <stella221125@gmail.com>
2023-10-27 20:47:00 +0900

    fix: フィード画面内のメニューからフォロー操作を行った場合、非ログイン時でもログインを促すダイアログが表示されない問題を修正

--- lib/widgets/list_tile/modal_follow_user_list_tile.dart
+++ lib/widgets/list_tile/modal_follow_user_list_tile.dart
@@ -1,7 +1,11 @@
 import 'package:aipictors/default.i18n.dart';
+import 'package:aipictors/providers/auth_user_id_provider.dart';
+import 'package:aipictors/providers/home_tab_index_provider.dart';
 import 'package:aipictors/utils/show_error_snack_bar.dart';
+import 'package:aipictors/widgets/dialog/about_follow_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ModalFollowUserListTile extends HookConsumerWidget {
@@ -17,6 +21,7 @@ class ModalFollowUserListTile extends HookConsumerWidget {
 
   @override
   Widget build(context, ref) {
+    final authUserId = ref.watch(authUserIdProvider);
     final state = useState(isActive);
 
     return ListTile(
@@ -29,11 +34,41 @@ class ModalFollowUserListTile extends HookConsumerWidget {
       ),
       onTap: () async {
         try {
-          state.value = !state.value;
-          await onTap();
+          if (authUserId.value == null) {
+            //context.pop();
+            return onShowLoginDialog(context, ref);
+          } else {
+            state.value = !state.value;
+            await onTap();
+          }
         } catch (exception) {
           showErrorSnackBar(context, exception);
         }
+      },
+    );
+  }
+
+  /// ログインを促すダイアログ
+  onShowLoginDialog(
+    BuildContext context,
+    WidgetRef ref,
+  ) async {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (_) {
+        return AboutFollowDialog(
+          onCancel: () {
+            context.pop();
+          },
+          onAccept: () {
+            // ログインのページに遷移する
+            final notifier = ref.read(homeTabIndexProvider.notifier);
+            notifier.toLoginTab();
+            context.pop();
+            context.pop();
+          },
+        );
       },
     );
   }
