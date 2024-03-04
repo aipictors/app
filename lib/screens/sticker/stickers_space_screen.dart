4dd90596bcbe4a1561a1e799e760e4e4133c1c90
stella <stella221125@gmail.com>
2024-03-05 03:23:04 +0900

    fix: StickersSpaceScreenの読み込み中に戻るボタンが表示されていた問題を修正

--- lib/screens/sticker/stickers_space_screen.dart
+++ lib/screens/sticker/stickers_space_screen.dart
@@ -2,11 +2,11 @@ import 'package:aipictors/default.i18n.dart';
 import 'package:aipictors/graphql/__generated__/stickers.req.gql.dart';
 import 'package:aipictors/providers/client_provider.dart';
 import 'package:aipictors/providers/config_provider.dart';
-import 'package:aipictors/screens/loading_screen.dart';
 import 'package:aipictors/widgets/app_bar/search_app_bar.dart';
 import 'package:aipictors/widgets/builder/operation_builder.dart';
 import 'package:aipictors/widgets/container/error/data_empty_error_container.dart';
 import 'package:aipictors/widgets/container/error/unexpected_error_container.dart';
+import 'package:aipictors/widgets/container/loading_container.dart';
 import 'package:aipictors/widgets/view/stickers_grid_view.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
@@ -27,7 +27,7 @@ class StickersSpaceScreen extends HookConsumerWidget {
     final config = ref.watch(configProvider);
 
     if (client.value == null) {
-      return const LoadingScreen();
+      return const LoadingContainer();
     }
 
     final searchContainer = SearchContainer(
