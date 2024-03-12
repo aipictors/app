7f2d663ea65456e59e5ad84d63fa9b60c6ceab73
stella <stella221125@gmail.com>
2024-03-12 13:19:06 +0900

    update: DailyThemesOperationBuilderを廃止する

--- lib/widgets/list_view/daily_themes_list_view.dart
+++ lib/widgets/list_view/daily_themes_list_view.dart
@@ -4,12 +4,15 @@ import 'package:aipictors/providers/config_provider.dart';
 import 'package:aipictors/screens/loading_screen.dart';
 import 'package:aipictors/utils/to_weekday.dart';
 import 'package:aipictors/widgets/builder/daily_themes_operation_builder.dart';
+import 'package:aipictors/widgets/builder/operation_builder.dart';
 import 'package:aipictors/widgets/container/error/data_empty_error_container.dart';
 import 'package:aipictors/widgets/container/error/data_not_found_error_container.dart';
 import 'package:aipictors/widgets/container/loading_container.dart';
 import 'package:aipictors/widgets/list_tile/daily_theme_list_tile.dart';
+import 'package:ferry/ferry.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -34,28 +37,51 @@ class DailyThemesListView extends HookConsumerWidget {
       return const LoadingScreen();
     }
 
-    // ページ切り替え時に前ページのデータがキャッシュで表示されるのを防ぐため、DailyThemesOperationBuilderを使用
-    // TODO: ferryのキャッシュ設定を変更し、DailyThemesOperationBuilderを廃止する
-    return DailyThemesOperationBuilder(
+    final useCache = useState(true);
+
+    final request = GDailyThemesReq((builder) {
+      return builder
+        ..fetchPolicy = FetchPolicy.CacheAndNetwork
+        ..vars.limit = config.graphqlQueryLimit
+        ..vars.offset = 0
+        ..vars.where.month = month
+        ..vars.where.year = year;
+    });
+
+    // ページ切り替え時に前ページのデータがキャッシュで表示されるのを防ぐため、キャッシュの有無を確認する
+    // TODO: ferryのキャッシュ設定を変更し、もっといい方法で修正する
+
+    // キャッシュの有無を確認する
+    final hasCache = client.value!.cache.readQuery(request);
+    if (hasCache == null) {
+      useCache.value = false;
+    } else {
+      useCache.value = true;
+    }
+
+    return OperationBuilder(
       client: client.value!,
-      operationRequest: GDailyThemesReq((builder) {
-        return builder
-          ..vars.limit = config.graphqlQueryLimit
-          ..vars.offset = 0
-          ..vars.where.month = month
-          ..vars.where.year = year;
-      }),
+      operationRequest: request,
       builder: (context, response) {
         final dailyThemeList = response.data?.dailyThemes;
         if (response.loading) {
           return const LoadingContainer();
         }
+
         if (dailyThemeList == null) {
           return const DataNotFoundErrorContainer();
         }
+
         if (dailyThemeList.isEmpty) {
           return const DataEmptyErrorContainer();
         }
+
+        // キャッシュがないのにDataSourceがキャッシュなら、異なる月のデータを表示しているので、DataSourceが変わるまで待つ
+        if (useCache.value == false &&
+            response.dataSource == DataSource.Cache) {
+          return const LoadingContainer();
+        }
+
         return ListView.builder(
           padding: const EdgeInsets.only(bottom: 16, left: 0, right: 0),
           itemCount: dailyThemeList.length,
