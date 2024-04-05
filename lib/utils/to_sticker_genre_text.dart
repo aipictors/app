e615ec2a7896cd03a11e7adc895483f6f0d42e80
stella <stella221125@gmail.com>
2024-04-05 22:03:36 +0900

    update: スタンプジャンルの多言語化

--- lib/utils/to_sticker_genre_text.dart
+++ lib/utils/to_sticker_genre_text.dart
@@ -5,6 +5,14 @@ String toStickerGenreText(GStickerGenre n) {
   switch (n) {
     case GStickerGenre.ANIMAL:
       return '動物'.i18n;
+    case GStickerGenre.BACKGROUND:
+      return '背景'.i18n;
+    case GStickerGenre.CHARACTER:
+      return '人物'.i18n;
+    case GStickerGenre.MACHINE:
+      return '機械'.i18n;
+    case GStickerGenre.OBJECT:
+      return '物'.i18n;
     default:
       return n.toString();
   }
