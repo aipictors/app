74fb46c3ffa0bf6a8bbdbb4f63c07d1fed2a39b1
stella <stella221125@gmail.com>
2024-04-13 10:00:52 +0900

    fix: wordpress版とサムネ位置がずれていた問題を修正

--- lib/widgets/image/grid_work_image.dart
+++ lib/widgets/image/grid_work_image.dart
@@ -29,38 +29,59 @@ class GridWorkImage extends HookConsumerWidget {
 
     return LayoutBuilder(builder: (context, constraints) {
       // thumbnailImagePositionはパーセントで渡されるので、Offsetに変換する
-      final double thumbnailOffset =
-          constraints.maxWidth * ((thumbnailImagePosition ?? 0) / 100);
+      double thumbnailOffset = 0;
+      if (imageAspectRatio! < 1.0) {
+        thumbnailOffset = constraints.maxWidth /
+            imageAspectRatio! *
+            (thumbnailImagePosition! / 100);
+      } else if (imageAspectRatio! > 1.0) {
+        thumbnailOffset = constraints.maxHeight *
+            imageAspectRatio! *
+            (thumbnailImagePosition! / 100);
+      }
+
+      if (thumbnailImagePosition == 0) {
+        thumbnailOffset =
+            (constraints.maxHeight / (imageAspectRatio ?? 1)) * (-5 / 100);
+      }
+      // 短編:長編 = 180:y  長編＊(短編/180)=y 180*aspectratio=長編  y*thumbnailpos=offset
 
       return FittedBox(
         fit: BoxFit.cover,
         // offsetは縦長画像の場合は一番上、横長画像の場合は一番左になる
-        alignment: ((imageAspectRatio ?? 0) <= 1.0)
-            ? Alignment.topCenter
-            : Alignment.topLeft,
+        alignment: Alignment.topLeft,
         // 画像が枠からはみ出さないようにclipBehaviorを指定。負荷が高ければClip.hardEdgeでいいかもしれない
         clipBehavior: Clip.antiAlias,
+
         child: Transform.translate(
           offset: ((imageAspectRatio ?? 0) <= 1.0)
               ? Offset(0, thumbnailOffset)
               : Offset(thumbnailOffset, 0),
-          child: CachedNetworkImage(
-            imageUrl: imageURL!,
-            progressIndicatorBuilder: (context, url, downloadProgress) {
-              return Container(
-                // double.infinityはエラーになってしまうので、MediaQueryを使用する
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                color: Theme.of(context).colorScheme.primaryContainer,
-              );
-            },
-            errorWidget: (context, uri, error) {
-              return Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                color: Theme.of(context).disabledColor,
-              );
-            },
+          child: SizedBox(
+            width: (imageAspectRatio! <= 1.0)
+                ? constraints.maxWidth
+                : constraints.maxWidth * imageAspectRatio!,
+            height: (imageAspectRatio! >= 1.0)
+                ? constraints.maxHeight
+                : constraints.maxHeight / imageAspectRatio!,
+            child: CachedNetworkImage(
+              imageUrl: imageURL!,
+              progressIndicatorBuilder: (context, url, downloadProgress) {
+                return Container(
+                  // double.infinityはエラーになってしまうので、MediaQueryを使用する
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                );
+              },
+              errorWidget: (context, uri, error) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  color: Theme.of(context).disabledColor,
+                );
+              },
+            ),
           ),
         ),
       );
