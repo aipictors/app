8e81d118e26e45c059c36c1309a87f7b31233335
stella <stella221125@gmail.com>
2024-04-30 00:35:40 +0900

    update: ImageGenerationProviderを追加

---
+++ lib/providers/image_generation_provider.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'image_generation_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$imageGenerationHash() => r'8d8b0e91721bd2d9be3c0237fc472f8a5cea1e7d';
+
+/// 設定
+///
+/// Copied from [ImageGeneration].
+@ProviderFor(ImageGeneration)
+final imageGenerationProvider =
+    AutoDisposeNotifierProvider<ImageGeneration, ImageGenerationState>.internal(
+  ImageGeneration.new,
+  name: r'imageGenerationProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$imageGenerationHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ImageGeneration = AutoDisposeNotifier<ImageGenerationState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
