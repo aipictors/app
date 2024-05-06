72982e8f3c8a66b817c8eafafce58312fef5919d
stella <stella221125@gmail.com>
2024-05-06 21:12:09 +0900

    update: レーティングボタンを追加

---
+++ lib/widgets/container/generation/generation_rating_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class GenerationRatingButton extends HookConsumerWidget {
+  const GenerationRatingButton(
+      {super.key, required this.isFilled, required this.onPressed});
+
+  final bool isFilled;
+
+  final Function() onPressed;
+
+  @override
+  Widget build(context, ref) {
+    return Row(children: [
+      IconButton(
+        onPressed: () {
+          onPressed();
+        },
+        icon: (isFilled)
+            ? const Icon(Icons.star_outline_rounded)
+            : Icon(
+                Icons.star_rounded,
+                color: Theme.of(context).colorScheme.inverseSurface,
+              ),
+      ),
+    ]);
+  }
+}
