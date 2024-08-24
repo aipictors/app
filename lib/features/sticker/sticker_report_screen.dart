597eff57bb659da693469f2f17304d70921176d6
stella <stella221125@gmail.com>
2024-08-25 01:45:38 +0900

    fix: 報告機能のビルドエラーを修正

--- lib/features/sticker/sticker_report_screen.dart
+++ lib/features/sticker/sticker_report_screen.dart
@@ -26,22 +26,102 @@ class StickerReportScreen extends HookConsumerWidget {
         ListTile(title: Text('該当する報告理由を選択してください。'.i18n)),
         ListTile(
           title: Text(
-            '誹謗中傷'.i18n,
+            '対象年齢が異なる'.i18n,
             style: const TextStyle(fontWeight: FontWeight.bold),
           ),
-          subtitle: Text('他人に誹謗中傷など嫌がらせをしている。'.i18n),
+          subtitle: Text('対象年齢が異なる（過度な性的表現など）'.i18n),
           onTap: () {
-            onReport(context, GReportReason.SLANDER);
+            onReport(context, GReportReason.AGE_MISMATCH);
           },
         ),
         ListTile(
           title: Text(
-            'スパム'.i18n,
+            '実写の児童ポルノの恐れがある'.i18n,
             style: const TextStyle(fontWeight: FontWeight.bold),
           ),
-          subtitle: Text('イラストとは関係のない商品のURLなどを大量に投稿している。'.i18n),
+          subtitle: Text('実写に見える作品で、児童ポルノと認定される恐れのある内容が含まれている'.i18n),
           onTap: () {
-            onReport(context, GReportReason.SPAM);
+            onReport(context, GReportReason.CHILD_PORNOGRAPHY);
+          },
+        ),
+        ListTile(
+          title: Text(
+            '商業用目的の投稿'.i18n,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text('商業用の広告や宣伝、勧誘を目的とする情報が含まれている'.i18n),
+          onTap: () {
+            onReport(context, GReportReason.COMMERCIAL_CONTENT);
+          },
+        ),
+        ListTile(
+          title: Text(
+            '過度なグロテスク表現'.i18n,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text('過度なグロテスク表現が含まれている'.i18n),
+          onTap: () {
+            onReport(context, GReportReason.EXCESSIVE_GORE);
+          },
+        ),
+        ListTile(
+          title: Text(
+            '禁止コンテンツへの誘導'.i18n,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text('サイトで禁止されているコンテンツへの誘導が含まれている'.i18n),
+          onTap: () {
+            onReport(context, GReportReason.ILLEGAL_CONTENT);
+          },
+        ),
+        ListTile(
+          title: Text(
+            'モザイク処理がされていない'.i18n,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text('必要なモザイク加工がされていない'.i18n),
+          onTap: () {
+            onReport(context, GReportReason.NO_MOSAIC);
+          },
+        ),
+        ListTile(
+          title: Text(
+            'プライバシー・肖像権の侵害'.i18n,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text('ライバシーまたは肖像権を侵害している'.i18n),
+          onTap: () {
+            onReport(context, GReportReason.PRIVACY_VIOLATION);
+          },
+        ),
+        ListTile(
+          title: Text(
+            'テイストが異なる'.i18n,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text('テイストが異なる'.i18n),
+          onTap: () {
+            onReport(context, GReportReason.TASTE_MISMATCH);
+          },
+        ),
+        ListTile(
+          title: Text(
+            '無断転載している'.i18n,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text('無断転載している'.i18n),
+          onTap: () {
+            onReport(context, GReportReason.UNAUTHORIZED_REPOST);
+          },
+        ),
+        ListTile(
+          title: Text(
+            'その他'.i18n,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text('その他'.i18n),
+          onTap: () {
+            onReport(context, GReportReason.OTHER);
           },
         ),
       ]),
