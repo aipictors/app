b90ee783f08b1a64569523667da45bab1415c098
stella <stella221125@gmail.com>
2024-08-25 23:55:36 +0900

    update：時刻の表記を変更

--- lib/utils/to_readable_date_time.dart
+++ lib/utils/to_readable_date_time.dart
@@ -2,7 +2,7 @@ import 'package:aipictors/default.i18n.dart';
 import 'package:intl/intl.dart';
 
 String toReadableDateTime(int time) {
-  final dateFormat = DateFormat('yyyy年MM月dd日H時m分'.i18n);
+  final dateFormat = DateFormat('yyyy年MM月dd日HH時mm分'.i18n);
 
   return dateFormat.format(
     DateTime.fromMillisecondsSinceEpoch(
--- lib/utils/to_readable_time.dart
+++ lib/utils/to_readable_time.dart
@@ -2,7 +2,7 @@ import 'package:aipictors/default.i18n.dart';
 import 'package:intl/intl.dart';
 
 String toReadableTime(int time) {
-  final dateFormat = DateFormat('MM月dd日H時m分'.i18n);
+  final dateFormat = DateFormat('MM月dd日HH時mm分'.i18n);
 
   return dateFormat.format(
     DateTime.fromMillisecondsSinceEpoch(
