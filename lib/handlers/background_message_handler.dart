44b265953a3da42eeb291a3f65ae51bcac563287
macsye <haleanab@gmail.com>
2026-01-25 06:39:49 +0900

    ライブラリapp_badge_plusを追加、flutter_app_badgerのDiscontinuedに伴う。

--- lib/handlers/background_message_handler.dart
+++ lib/handlers/background_message_handler.dart
@@ -1,8 +1,8 @@
 import 'package:aipictors/repositories/config_repository.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter_app_badger/flutter_app_badger.dart';
+import 'package:app_badge_plus/app_badge_plus.dart';
 
 Future<void> backgroundMessageHandler(RemoteMessage message) async {
   final count = const ConfigRepository().badgeCount + 1;
-  FlutterAppBadger.updateBadgeCount(count);
+  await AppBadgePlus.updateBadge(count);
 }
